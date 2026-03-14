bb1c8d3438bf03eba0874af26514ce2a65ecefcf
rajola <kakanderaymond159@gmail.com>
2026-03-14 14:17:21 +0300

    Add files via upload

---
+++ Logout.js
@@ -0,0 +1,14 @@
+// Created by rajola//
+
+window.addEventListener('load', ()=>{
+const labels = document.querySelectorAll('.form-control label');
+
+labels.forEach(label =>{
+    label.innerHTML = label.innerText
+    .split('')
+    .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+    .join('');
+});
+
+
+})
